35cae096f40f04ffc4b3eae1340fd7d16f6bfcb8
Antonio <00267016@uca.edu.sv>
2020-04-07 00:12:23 -0600

    project added

---
+++ src/test/java/com/example/user/laboratorio2/Laboratorio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anton.laboratorio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
